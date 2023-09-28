b80adefeb992c17079860b6042075e625ec0458d
marioblacar <marioblascocarrion01@gmail.com>
2023-09-28 09:16:02 +0200

    commit 28/09/23

--- unidad02/js/PROG203.js
+++ unidad02/js/PROG203.js
@@ -6,7 +6,7 @@ let precioPlatano = prompt("escriba el precio por kilo de platanos");
 let cantidadOPlatano = prompt("escriba la cantidad de platanos");
 
 let totalKilos = cantidadManzana + cantidadNaranja + cantidadOPlatano;
-let totalPrecio = precioManzana * 0.5 + precioNaranja * 0.3 + precioPlatano * 0.8;
+let totalPrecio = precioManzana * cantidadManzana + precioNaranja * cantidadNaranja + precioPlatano * cantidadOPlatano;
 
 let datos =  (`Has comprado un total de ${totalKilos} kilos de fruta y te ha costado ${totalPrecio} euros.`);
 alert(datos);

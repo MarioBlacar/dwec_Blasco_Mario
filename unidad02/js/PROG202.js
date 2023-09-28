a5a1bf8f4944ab63dd769bfd5726b2a35976e3d4
marioblacar <marioblascocarrion01@gmail.com>
2023-09-28 09:16:40 +0200

    commit 28/09/23

--- unidad02/js/PROG202.js
+++ unidad02/js/PROG202.js
@@ -1,7 +1,7 @@
 let nombreUsuario = prompt("escriba el nombre de usuario");
 let nota1 = prompt("escriba la nota de su primer examen");
 let nota2 = prompt("escriba la nota de su segundo examen");
-let notaMedia = (nota1+nota2)/2;
+let notaMedia = (Number(nota1)+Number(nota2))/2;
 
 let datos =  (`Hola ${nombreUsuario}, la nota media de tus exámenes es ${notaMedia}.`);
 alert(datos);
